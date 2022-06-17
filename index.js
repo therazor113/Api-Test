95cf53f26fcad1acf56a7c82c2b6fd0588357879
Razor <therazor113@gmail.com>
2022-06-17 12:35:35 -0400

    clean start

--- index.js
+++ index.js
@@ -3,47 +3,10 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res, next) => {
-  res.send('root')
+  res.send('Hello World!')
   next()
 })
-app.use('/taco/([\$])tacobell', (req, res, next) => {
-  res.redirect('test/taco')
-  console.log('Time:', Date.now())
-  next()
-})
-app.get('/taco/test/:test', (req, res) => {
-  res.send(req.params)
-})
 
-
-const cb0 = (req, res, next) => {
-  console.log('CB0')
-  next()
-}
-
-const cb1 = (req, res, next) => {
-  console.log('CB1')
-  next()
-}
-
-app.get('/example/d', [cb0, cb1], (req, res, next) => {
-  console.log('the response will be sent by the next function ...')
-  next()
-}, (req, res) => {
-  res.send('Hello from D!')
-})
-
-app.route('/book')
-  .get((req, res) => {
-    res.send('Get a random book')
-  })
-  .post((req, res) => {
-    res.send('Add a book')
-  })
-  .put((req, res) => {
-    res.send('Update the book')
-  })
-  
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
